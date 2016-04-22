bba4d382de65e1d686061d42de9d1c5c59270f59
Edwin Perez <edwinperez539@gmail.com>
2016-04-22 16:27:07 -0400

    Optimizing js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,49 @@
-var path = require('path');
+var path    = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: {
-    app: ['./src/js/main.js']
-  },
-  output: {
-    filename: './build/js/bundle.js'
-  },
-  progress: true,
-  colors: true,
-  inline: true,
+	entry: {
+		app: ['./src/js/main.js']
+	},
+	output: {
+		filename: './build/js/bundle.js'
+	},
+	progress: true,
+	colors: true,
+	inline: true,
 	resolve: {
 		moduleDirectories: ['./node_modules/', './src/js/'],
 		alias: {
 			bootstrap: path.join(__dirname, 'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js')
 		}
 	},
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: /node_modules/,
-        query: {
-          cacheDirectory: true,
-          presets: ['es2015', 'react', 'stage-0']
-        }
-      }
-    ]
-  }
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel',
+				exclude: /node_modules/,
+				query: {
+					cacheDirectory: true,
+					presets: ['es2015', 'react', 'stage-0']
+				}
+			}
+		]
+	},
+	plugins: [
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.OccurrenceOrderPlugin(true),
+		new webpack.optimize.UglifyJsPlugin({
+			mangle: true,
+			output: {
+				comments: false
+			},
+			compress: {
+				warnings: false
+			}
+		}),
+		new webpack.DefinePlugin({
+				'process.env.NODE_ENV': '"production"'
+		}),
+	]
 };
